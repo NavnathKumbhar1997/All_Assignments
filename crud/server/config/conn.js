373ccc44e26386ed664e449fed9fc21e9e40d8b5
NavnathKumbhar1997 <kumbharnr1997@gmail.com>
2022-05-02 12:21:20 +0530

    newly added

--- crud/server/config/conn.js
+++
@@ -1,9 +0,0 @@
-const { default: mongoose } = require('mongoose')
-const mogoose = require('mongoose')
-const DB = "mongodb+srv://crudeTest:WIy78IILrAytb5ln@cluster0.d5gxo.mongodb.net/mern_stack?retryWrites=true&w=majority"
-
-mongoose.connect(DB,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}).then(()=>console.log("connection succesfull")).catch((err)=>console.log(err.message));
-
--- strappp/my-project/config/admin.js
+++
@@ -1,5 +0,0 @@
-module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET', '046752834aa3310404bb8fe36c897bb5'),
-  },
-});
